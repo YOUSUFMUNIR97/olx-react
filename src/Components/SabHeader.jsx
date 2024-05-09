35c817e9ee7e2c087d09b5d81fd1f2991531e87b
wajeehamasood570 <wajeehamasood570@gmail.com>
2024-05-09 21:21:42 +0500

    signup modal

--- src/Components/SabHeader.jsx
+++ src/Components/SabHeader.jsx
@@ -18,7 +18,6 @@ import MailIcon from "@mui/icons-material/Mail";
 import PhoneIcon from "@mui/icons-material/Phone";
 import CloseIcon from "@mui/icons-material/Close";
 
-
 const SabHeader = () => {
   const fbsvgData = `<svg
   //           className="m-2"
@@ -53,7 +52,8 @@ const SabHeader = () => {
   };
 
   const [searchTerm, setSearchTerm] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false); // State to manage modal visibility
+  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false); // State to manage modal visibility
 
   const navigate = useNavigate();
 
@@ -63,12 +63,19 @@ const SabHeader = () => {
   };
 
   const handleLoginClick = () => {
-    setIsModalOpen(true); // Open the modal when "Login" is clicked
-    console.log(isModalOpen);
+    setIsSignUpModalOpen(false);
+    setIsLoginModalOpen(true); // Open the modal when "Login" is clicked
+    console.log(isLoginModalOpen);
+  };
+
+  const handleSignupClick = () => {
+    setIsSignUpModalOpen(true); // Open the modal when "SignUp" is clicked
+    console.log(isSignUpModalOpen);
   };
 
   const handleCloseModal = () => {
-    setIsModalOpen(false); // Close the modal
+    setIsLoginModalOpen(false); // Close the modal
+    setIsSignUpModalOpen(false); // Close the modal
   };
 
   // const handleclick = () => {
@@ -145,10 +152,10 @@ const SabHeader = () => {
         <SMSellButton />
       </div>
 
-      {/* Modal Component */}
+      {/* Login Modal Component */}
       <Modal
         keepMounted
-        open={isModalOpen}
+        open={isLoginModalOpen}
         onClose={handleCloseModal}
         aria-labelledby="keep-mounted-modal-title"
         aria-describedby="keep-mounted-modal-description"
@@ -207,8 +214,89 @@ const SabHeader = () => {
             icon={<PhoneIcon color="#002f34" />}
             label="Sign in with Phone"
           />
-          
-          <Button className="my-2" style={{color:"#3a77ff", fontWeight:"700" }}> New to OLX ? Create an Account </Button>
+
+          <Button
+            className="my-2"
+            style={{ color: "#3a77ff", fontWeight: "700" }}
+            onClick={handleSignupClick}
+          >
+            {" "}
+            New to OLX ? Create an Account{" "}
+          </Button>
+        </Box>
+      </Modal>
+
+      {/* Signup Modal Component */}
+      <Modal
+        keepMounted
+        open={isSignUpModalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={style}>
+          <IconButton
+            aria-label="close"
+            onClick={handleCloseModal}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+
+          <img
+            src="https://www.olx.com.pk/assets/brandIconLogin_noinline.93e8a1a9cf50902ba5250814f57810ff.svg"
+            height="50px"
+            style={{ margin: "15px" }}
+          />
+          <Typography
+            id="keep-mounted-modal-title"
+            variant="h6"
+            component="h1"
+            style={{ fontWeight: "700", lineHeight: "3.6" }}
+          >
+            Login into your OLX account
+          </Typography>
+
+          {/* <button type="button" style={{backgroundColor:"blue"}} className="text-white bg-blue-900 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
+                        <svg className='m-2' style={{width:"20px", height:"20px"}} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                            <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd" />
+                        </svg>
+                        Sign in with Facebook
+                    </button> */}
+
+          <AuthenticationButton
+            icon={<FacebookIcon color="primary" />}
+            label="Join with Facebook"
+          />
+          <AuthenticationButton
+            icon={<GoogleIcon color="success" />}
+            label="Join with Google"
+          />
+
+          <p className="my-2"> OR </p>
+
+          <AuthenticationButton
+            icon={<MailIcon color="#002f34" />}
+            label="Join with Email"
+          />
+          <AuthenticationButton
+            icon={<PhoneIcon color="#002f34" />}
+            label="Join with Phone"
+          />
+
+          <Button
+            className="my-2"
+            style={{ color: "#3a77ff", fontWeight: "700" }}
+            onClick={handleLoginClick}
+          >
+            {" "}
+            Already have an Account ? Log in{" "}
+          </Button>
         </Box>
       </Modal>
     </div>
